626a93be6f9a0ceb0bbc95e49121c6525b4baa9f
mnv <mnvx@yandex.ru>
2017-06-18 22:46:19 +0300

    translation for userextended

---
+++ lang/ru/clake/userextended/lang.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'components' => [
+        'account' => [
+            'password' => 'Пароль',
+            'signin' => 'Войти',
+            'logout' => 'Выйти',
+            'firstname' => 'Имя',
+            'lastname' => 'Фамилия',
+            'email' => 'Email',
+            'confirmpassword' => 'Повторите пароль',
+            'register' => 'Зарегистрировать',
+            'enterfname' => 'Укажите имя',
+            'enterlname' => 'Укажите фамилию',
+            'enteremail' => 'Укажите Email',
+            'enterpassword' => 'Укажите пароль',
+            'fullname' => 'Полное имя',
+            'newpassword' => 'Новый пароль',
+            'confirmnewpassword' => 'Confirm New Password',
+            'save' => 'Сохранить',
+            'timezone' => 'Временная зона',
+        ],
+        'friend' => [
+            'accept' => 'Accept',
+            'lastonline' => 'Last Online',
+        ],
+        'user' => [
+            'writtenat' => 'Written',
+            'search' => 'Search',
+        ]
+    ]
+];
